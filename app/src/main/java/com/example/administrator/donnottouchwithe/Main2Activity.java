963fd9feadcd79bd77cd084fcb327812eeb746e8
zhouyi <513466543@qq.com>
2016-09-12 22:00:32 +0800

    1.1
    删掉了一些不必要的数据库连接代码，并添加了详细的注释。

--- app/src/main/java/com/example/administrator/donnottouchwithe/Main2Activity.java
+++ app/src/main/java/com/example/administrator/donnottouchwithe/Main2Activity.java
@@ -1,42 +1,29 @@
 package com.example.administrator.donnottouchwithe;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
     private TextView tv_df_1;
     private TextView tv_gf;
-    private SQLiteDatabase db;
-    private int gf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        MySqlite ms = new MySqlite(this);
-        db = ms.getWritableDatabase();
-
-        Cursor cursor = db.query("fenshu",null,null,null,null,null,null,null);
-        while (cursor.moveToNext()){
-            gf = cursor.getInt(cursor.getColumnIndex("fs"));
-        }
-
         tv_df_1 = (TextView) findViewById(R.id.tv_df_1);
         tv_gf = (TextView) findViewById(R.id.tv_gf);
-
+        //获取当前最高分
         Intent intent = getIntent();
         String df = intent.getStringExtra("df");
+        String gf = intent.getStringExtra("gf");
         tv_df_1.setText(df);
-        tv_gf.setText(gf+"");
+        tv_gf.setText(gf);
     }
 
     public void click(View v){
--- app/src/main/java/com/example/administrator/donnottouchwithe/MainActivity.java
+++ app/src/main/java/com/example/administrator/donnottouchwithe/MainActivity.java
@@ -31,24 +31,26 @@ public class MainActivity extends AppCompatActivity {
     private Timer timer1 = new Timer();
     private Timer timer2 = new Timer();
 
-
     static Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
 //            super.handleMessage(msg);
             if (msg.what==0){
-                flag = true;
+                flag = true;//判断是否可以开始计时，即玩家第一次点击
                 df = df + 1;
-                tv_df.setText(df+"");
+                tv_df.setText(df+"");//将得分实时显示到组件上
+                //将所有组件下移实现变化效果
                 for (int i = 3;i > 0;i--){
                     for (int j = 0;j<fklenth;j++){
                         fk[i][j].setI(fk[i-1][j].getI());
                     }
                 }
+                //下移后重新设置第一行的属性
                 for (int j = 0;j<fklenth;j++){
                     fk[0][j].setI(2);
                 }
                 fk[0][(int) (Math.random()*3)].setI(0);
+                //将最后一行的白块设置为可点击状态
                 for (int j = 0; j < fklenth; j++) {
                     if (fk[3][j].getI() == 0){
                         fk[3][j].setCanclick(0);
@@ -69,8 +71,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //应用创建时，创建数据库
         MySqlite ms = new MySqlite(this);
         db = ms.getWritableDatabase();
+        //查询出最高分，用于与当前分数比较
         Cursor cursor = db.query("fenshu",null,null,null,null,null,null,null);
         while (cursor.moveToNext()){
             gf = cursor.getInt(cursor.getColumnIndex("fs"));
@@ -92,19 +96,21 @@ public class MainActivity extends AppCompatActivity {
                 fk[i][j] = new Fangkuai(this);
                 ll[i].addView(fk[i][j]);
             }
+            //随机设置每一行的白格
             int randnum = (int) (Math.random()*3);
             fk[i][randnum].setI(0);
         }
-
+        //将最后一行的白块设置为可点击状态
         for (int j = 0; j < fklenth; j++) {
             if (fk[3][j].getI() == 0){
                 fk[3][j].setCanclick(0);
             }
         }
+        //设置计时器监听flag（即游戏开始状态）
         timer1.schedule(timerTask,0,100);
     }
 
-
+    //计时器任务，flag为true后，开起倒计时，并将该计时器废弃
     TimerTask timerTask = new TimerTask() {
         @Override
         public void run() {
@@ -114,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-
+    //计时任务，为游戏的倒计时计时
     TimerTask task = new TimerTask() {
         @Override
         public void run() {
@@ -124,11 +130,15 @@ public class MainActivity extends AppCompatActivity {
                     if (time==0){
                         timer2.cancel();
                         if (df > gf) {
+                            //将高分与该局得分相比，如果高则添加到数据库中
                             db.execSQL("update fenshu set fs = ? where _id = ?", new Object[]{df, 1});
                         }
+                        //显性跳转到另外一个布局文件中
                         Intent it = new Intent();
                         it.setClass(MainActivity.this,com.example.administrator.donnottouchwithe.Main2Activity.class);
+                        //将该局得分传递过去
                         it.putExtra("df",df+"");
+                        it.putExtra("gf",gf+"");
                         startActivity(it);
                         finish();
                     }
@@ -144,7 +154,9 @@ public class MainActivity extends AppCompatActivity {
         if (timer2!=null){
             timer2.cancel();
         }
+        //页面销毁时，将静态得分设置为0；
         df = 0;
+        //页面销毁时，将游戏开始状态设置为false
         flag = false;
         if (db!=null){
             db.close();
